90e29fd461aaedd848da82611f606cba363e0866
KityLinn <87649871+KityLinn@users.noreply.github.com>
2025-11-02 00:13:01 +0100

    changed about me text

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -17,11 +17,15 @@ export default function AboutSection() {
           </Col>
           <Col md={6}>
             <h3 className="mb-3 fw-bold">Linn Kristine Vikse</h3>
-            <p className="lead text-muted mb-3">
-              Front-end Developer
-            </p>
+            <p className="lead text-muted mb-3">Front-end Developer</p>
             <p className="mb-4 lh-lg">
-             Newly educated front-end developer with a passion for creating beautiful and innovate websites
+              I’m a newly educated frontend developer with a passion for
+              creating user-friendly and visually elegant websites. I
+              enjoy bringing designs to life using tools like React, JavaScript,
+              Bootstrap, and SASS, focusing on responsive layouts and clean,
+              maintainable code. I’m excited to continue learning, building, and
+              crafting digital experiences that make the web more intuitive and
+              enjoyable for everyone.
             </p>
             <div className="d-flex flex-column gap-3">
               <div className="d-flex align-items-center gap-3">
