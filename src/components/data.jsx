f7f013668eea3e767d6f9d45920b3fa5512528af
KityLinn <87649871+KityLinn@users.noreply.github.com>
2025-11-02 21:37:04 +0100

    updated with pictures

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,9 +1,14 @@
+import holidaze from '../assets/holidaze.png';
+import mebay from '../assets/mebay.png'
+import science from '../assets/science.png'
+import game from '../assets/gamehub.png'
+
 export const data = [
   {
     title: 'Holidaze',
     description: 'In this project I was tasked with creating an online booking site using a provided API. It allows users to view a list of venues as well as individual venues, search through venues, create a new venue and edit one. It is a primarily react based application using a series of dependencies listed below. Boostrap, scss',
     technologies: ['React', 'Bootstrap', 'Sass',],
-    image: 'https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=400',
+    image: holidaze,
     githubLink: 'https://github.com/KityLinn/Linn-Kristine-Exam-2',
     pageLink: 'https://soft-daffodil-3e238f.netlify.app/login'
   },
@@ -11,7 +16,7 @@ export const data = [
     title: 'Me Bay',
     description: 'In this project i was tasked with create an online auction website using the provided API. Allowing users to look for bids and put bids out on items. It uses html, css, js as well as boostrap and SASS.orative project management tool with drag-and-drop functionality, real-time updates, and team collaboration features.',
     technologies: ['Bootstrap', 'CSS', 'API'],
-    image: 'https://images.pexels.com/photos/3184292/pexels-photo-3184292.jpeg?auto=compress&cs=tinysrgb&w=400',
+    image: mebay,
     githubLink: 'https://github.com/KityLinn/Auction-website',
     pageLink: 'https://kitylinn.github.io/Auction-website/'
   },
@@ -19,7 +24,7 @@ export const data = [
     title: 'Bergen Science Museum',
     description: 'Bergen Science Museum is a Website created for a fictional museum. Designed to appeal to families and their children, showing off the museums various exhubitis and attractions. It is created using html and css, creating a responsive and apealing design',
     technologies: ['HTML', 'CSS',],
-    image: 'https://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg?auto=compress&cs=tinysrgb&w=400',
+    image: science,
     githubLink: 'https://github.com/KityLinn/Science-museum',
     pageLink: 'https://gleaming-gnome-44b42a.netlify.app/'
   },
@@ -27,7 +32,7 @@ export const data = [
     title: 'Game Hub',
     description: 'For this project I was tasked with creating a simple online store using basic css and html.',
     technologies: ['HTML', 'CSS'],
-    image: 'https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=400',
+    image: game,
     pageLink: 'https://stupendous-buttercream-6a2724.netlify.app'
   },
   
